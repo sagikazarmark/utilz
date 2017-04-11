66965933b445663ba8599cafdf657557a7381d90
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-04-11 13:03:20 +0200

    Add spinal case conversion

--- strings/case.go
+++ strings/case.go
@@ -8,6 +8,16 @@ import (
 
 // ToSnake converts a string (camel or spinal) to snake case.
 func ToSnake(s string) string {
+	return toSeparated(s, '_')
+}
+
+// ToSpinal converts a string (camel or snake) to spinal case.
+func ToSpinal(s string) string {
+	return toSeparated(s, '-')
+}
+
+// toSeparated converts a string (camel, snake or spinal) to a lower-cased separated one (essentially snake or spinal based on the separator).
+func toSeparated(s string, sep rune) string {
 	// Skip processing for an empty string
 	if len(s) == 0 {
 		return ""
@@ -16,8 +26,8 @@ func ToSnake(s string) string {
 	// Build the results in this buffer
 	buf := new(bytes.Buffer)
 
-	// Trick: if the first rune is uppercase, do not prepend an underscore
-	prev := '_'
+	// Trick: if the first rune is uppercase, do not prepend a separator
+	prev := sep
 
 	for len(s) > 0 {
 		r, size := utf8.DecodeRuneInString(s)
@@ -25,26 +35,26 @@ func ToSnake(s string) string {
 
 		switch {
 		case unicode.IsUpper(r):
-			var uc bool
+			var writeSep bool
 
 			if len(s) > 0 {
 				next, _ := utf8.DecodeRuneInString(s)
-				uc = prev != '_' && (!unicode.IsUpper(prev) || (unicode.IsUpper(prev) && !unicode.IsUpper(next)))
+				writeSep = prev != sep && (!unicode.IsUpper(prev) || (unicode.IsUpper(prev) && !unicode.IsUpper(next)))
 			} else {
-				uc = prev != '_' && !unicode.IsUpper(prev)
+				writeSep = prev != sep && !unicode.IsUpper(prev)
 			}
 
-			// Prepend an underscore if the previous char is not an underscore
+			// Prepend a separator if the previous char is not a separator
 			// and the current char is not part of an abbreviation
-			if uc {
-				buf.WriteRune('_')
+			if writeSep {
+				buf.WriteRune(sep)
 			}
 
 			buf.WriteRune(unicode.ToLower(r))
 
 		default:
-			if r == '-' || r == ' ' {
-				r = '_'
+			if r == '_' || r == '-' || r == ' ' {
+				r = sep
 			}
 
 			buf.WriteRune(r)
--- strings/case_test.go
+++ strings/case_test.go
@@ -11,6 +11,7 @@ func TestToSnake(t *testing.T) {
 		"foo":       "foo",
 		"FooBar":    "foo_bar",
 		"fooBar":    "foo_bar",
+		"Foo_Bar":   "foo_bar",
 		"Foo-Bar":   "foo_bar",
 		"Foo Bar":   "foo_bar",
 		"FOOBar":    "foo_bar",
@@ -22,6 +23,27 @@ func TestToSnake(t *testing.T) {
 	for in, want := range data {
 		if got := strings.ToSnake(in); got != want {
 			t.Errorf("converting '%s' to snake case failed, expected: %s, actual: %s", in, want, got)
+		}
+	}
+}
+
+func TestToSpinal(t *testing.T) {
+	data := map[string]string{
+		"foo":       "foo",
+		"FooBar":    "foo-bar",
+		"fooBar":    "foo-bar",
+		"Foo_Bar":   "foo-bar",
+		"Foo-Bar":   "foo-bar",
+		"Foo Bar":   "foo-bar",
+		"FOOBar":    "foo-bar",
+		"FOOBarBaz": "foo-bar-baz",
+		"FOOBarBAZ": "foo-bar-baz",
+		"Foo_-Bar":  "foo--bar",
+	}
+
+	for in, want := range data {
+		if got := strings.ToSpinal(in); got != want {
+			t.Errorf("converting '%s' to spinal case failed, expected: %s, actual: %s", in, want, got)
 		}
 	}
 }
